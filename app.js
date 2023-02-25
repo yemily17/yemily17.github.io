fb513ac38cf90cafd97eb19d79f6647ab5c01e42
yemily17 <46946140+yemily17@users.noreply.github.com>
2023-02-24 20:01:45 -0500

    g

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 let d = new Date();
 document.body.innerHTML = "<h1>Today's date is " + d + "</h1>"
+
+var echoDB;
+
+// Query echo3D
+fetch('https://api.echo3D.com/query?key=' + API_KEY)
+.then((response) => response.json())
+.then((json) => {
+  // Store database
+  echoDB = json; // The JSON response
+})
+.catch((error) => {
+  console.error(error);
+});
+
+var apiKey = echoDB.apiKey;
+var db = Object.values(echoDB.db);
+
+document.body.innerHTML = "<h1>Today's date is " + apiKey + "</h1>"
